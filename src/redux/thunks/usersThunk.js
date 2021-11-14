44987fcdea371396bdb37a3d745586bb0b2b6fd0
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-11-14 18:37:19 +0100

    Added endpoint /home to get the users on loadUsersThunk

--- src/redux/thunks/usersThunk.js
+++ src/redux/thunks/usersThunk.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import { loadUsersAction } from "../actions/usersActionCreators";
 
 export const loadUsersThunk = () => async (dispatch) => {
-  const { data: robots } = await axios.get(process.env.REACT_APP_API_URL);
-  dispatch(loadUsersAction(robots));
+  const { data: users } = await axios.get(
+    process.env.REACT_APP_API_URL + "/home"
+  );
+  dispatch(loadUsersAction(users));
 };
