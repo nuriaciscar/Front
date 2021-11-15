f7a5ce210798ac961503c114a9b1cecf0571d571
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-11-15 21:16:58 +0100

    Changed url api on env

--- src/pages/LoginPage/LoginPage.test.js
+++ src/pages/LoginPage/LoginPage.test.js
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import configureStore from "../../redux/store/index";
 import { BrowserRouter as Router } from "react-router-dom";
 
-describe("Given a LoginPage omponent", () => {
+describe("Given a LoginPage component", () => {
   describe("When it's rendered", () => {
     test("Then it should render the LoginPage", async () => {
       const store = configureStore();
